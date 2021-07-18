7bd0c42e72c7342d5979ca83632bfe921c190a18
Becca Wyland <rwyland@uwalumni.com>
2021-07-18 13:21:37 -0500

    declare employee class

---
+++ index.js
@@ -0,0 +1,25 @@
+// declare the class Employee
+class Employee {
+
+    constructor(name, id, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+    
+    getEmily() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+}
