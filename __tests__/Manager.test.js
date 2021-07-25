67bc374a0e50b44a7f4c7926106df0017fb8e658
Becca Wyland <rwyland@uwalumni.com>
2021-07-25 16:17:41 -0500

    write manager.test.js

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@jest/globals');
+const Manger = require('../lib/Manager');
+
+test('checks if each manager has office number', () => {
+    const manOfficeNum = "189";
+    const manager = new Manager(1, "Becca", "becca@becca.com", manOfficeNum);
+    expect(manager.officeNumber).toEqual(expect.any(String));
+    expect(manager.getOfficeNumber().toBe(manOfficeNum))
+})
+
+test('checks manager role', () => {
+    const manRole = "Manager";
+    const manager = new Manager(1, "Becca", "becca@becca.com");
+    expect(manager.getRole()).toBe(manRole)
+})
