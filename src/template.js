cd824510d5463972960c39704b7ce632943d6c01
Becca Wyland <rwyland@uwalumni.com>
2021-07-25 17:09:18 -0500

    write module.exports for the html template

--- src/template.js
+++ src/template.js
@@ -1,4 +1,4 @@
-const team = data => {
+const generateTeam = teamHtml => {
     const teamManager = manager => {
         // get bootstrap for cards
         return `
@@ -33,3 +33,27 @@ const team = data => {
         `
     }    
 }
+
+
+
+module.exports = teamHtml => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+                <link rel="stylesheet" href="style.css">
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+                <title>Team Profile</title>
+            </head>
+        
+            <body>
+                
+            </body>
+        </html>
+    `;
+};
